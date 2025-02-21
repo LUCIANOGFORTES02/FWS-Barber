a961113fa52ddcbf13b24c6a9ffce6b994b8005f
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-02-20 23:57:27 -0300

    feat:  improving tehe style of the footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,15 +1,9 @@
-import { Card, CardContent } from "./ui/card"
-
 const Footer = () => {
   return (
-    <footer>
-      <Card>
-        <CardContent className="px-5 py-6">
-          <p className="text-sm text-gray-400">
-            © 2023 Copyright <span className="font-bold">FSW Barber</span>
-          </p>
-        </CardContent>
-      </Card>
+    <footer className="w-full bg-secondary px-5 py-6">
+      <p className="text-xs font-bold text-gray-400 opacity-75">
+        © 2023 Copyright FSW Barber
+      </p>
     </footer>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
+    <html lang="en">
+      <body className={`${inter.className} dark`}>
         <AuthProvider>
           <div className="flex-1">{children}</div>
           <Footer />
